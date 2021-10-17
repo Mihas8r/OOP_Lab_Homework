146cd6097684d7e42e8daa432d4fd94d4795b857
Mihaela <neacsu.mihaela.s8r@student.ucv.ro>
2021-10-17 21:28:10 +0300

    T2.12

---
+++ T2.12/T2.12/T2.12.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+template
+struct Cat
+{ 
+    string name;
+    string master;
+    int age;
+};
+struct Person
+{
+    string name;
+    int age;
+};
+struct Car
+{
+    string producer;
+    int licenseNumber;
+    int age;
+};
+int main()
+{
+    
+}
