8639a8527403a26e1aec091af14021711f4211dc
Mihaela <neacsu.mihaela.s8r@student.ucv.ro>
2021-11-02 17:08:31 +0200

    T5.3
    I solved T5.3 only for class A from T5.1 for a better understanding

---
+++ T5.3/T5.3/classA.cpp
@@ -0,0 +1,10 @@
+#include "classA.h"
+using namespace std;
+
+	void classA::testA()
+	{
+		aa = 4;
+		ab = 8;
+		ac = 5;
+	}
+
---
+++ T5.3/T5.3/classA.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class classA
+{
+	
+	public:
+		int aa;
+	protected:
+		int ab;
+	private:
+		int ac;
+	public:
+		void testA();
+			
+	
+};
+
